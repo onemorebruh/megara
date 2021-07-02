ef5b9aa452523549bfb10d74fc221384a038c446
ira <rybakov.i.a.2002@gmail.com>
2021-07-02 18:08:24 +0200

    db is connected to django

--- main/models.py
+++ main/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class remarks_users(models.Model):
+    # fields
+    username = models.CharField(primary_key=True, max_length=255, help_text="unique username")
+    password = models.CharField(max_length=255, help_text="password")
+
+    # metadata
+    class Meta:
+        ordering = ["username"]
+
+    # methods
+    def get_absolute_url(self):
+        return reverse('remarks-detail', args=[str(self.id)]) # почему это работает и помечено как ошибка одновременно
+
+    def __str__(self):
+        return self.username
